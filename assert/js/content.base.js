347dc3faf75bd708ebc30027ad03f53a122faab0
sasumi <cnsasumi@gmail.com>
2020-09-22 16:29:51 +0800

    upd

---
+++ assert/js/content.base.js
@@ -0,0 +1,493 @@
+(function(){
+	console.log('content script running');
+	let tmp = /tapd\.cn\/(\w+)\//.exec(location.href);
+	const WORKSPACE_ID = tmp ? tmp[1] : null;
+
+	const ONE_DAY = 86400;
+	const ONE_HOUR = 3600;
+	const ONE_MIN = 60;
+
+	let _CACHE_ = {};
+	let cache = (key, fetcher)=>{
+		return new Promise(resolve => {
+			if(_CACHE_[key]){
+				console.info('Cache hit', key);
+				return resolve(_CACHE_[key]);
+			}
+			fetcher().then(rsp=>{
+				_CACHE_[key] = rsp;
+				resolve(_CACHE_[key]);
+			});
+		});
+	};
+
+	const num_pad_left = (num)=>{
+		if(num < 10){
+			return '0'+num;
+		}
+		return num;
+	};
+
+	const prettyTimeRange = (misec) => {
+		let seconds = misec/1000;
+		let str = [];
+		if(seconds > ONE_DAY){
+			let d = Math.floor(seconds / ONE_DAY);
+			str.push(`${d}天`);
+			seconds -= d * ONE_DAY;
+		}
+		if(seconds > ONE_HOUR){
+			let h = Math.floor(seconds / ONE_HOUR);
+			str.push(`${h}小时`);
+			seconds -= h * ONE_HOUR;
+		}
+		if(seconds > ONE_MIN){
+			let m = Math.floor(seconds / ONE_MIN);
+			str.push(`${m}分钟`);
+			seconds -= m * seconds;
+		}
+		return str.join(' ');
+	};
+
+	const escapeHtml = (str = '')=>{
+		return str.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g,'&gt;')
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, '&#39;');
+	};
+
+	/**
+	 * @param {Date} date_obj
+	 * @param fmt
+	 */
+	const formatDate = (date_obj, fmt) => {
+		let pt = {
+			'Y': date_obj.getUTCFullYear(),
+			'm': num_pad_left(date_obj.getMonth() + 1),
+			'd': num_pad_left(date_obj.getUTCDate()),
+			'H': num_pad_left(date_obj.getUTCHours()),
+			'i': num_pad_left(date_obj.getUTCMinutes()),
+			's': num_pad_left(date_obj.getUTCSeconds()),
+		};
+		let ret = '';
+		for(let i=0; i<fmt.length; i++){
+			ret += pt[fmt[i]] || fmt[i];
+		}
+		return ret;
+	};
+
+	const STATUS_ORDERS = [
+		'待评估', '已核实', '处理中', '已验证', '待回电', '已解决', '已关闭', '已拒绝',
+		'已转需求', '评审中', '待上线', '拒绝需求'
+	];
+
+	const calcIndex = (status_text)=>{
+		for(let i=0; i<STATUS_ORDERS.length; i++){
+			if(status_text.indexOf(STATUS_ORDERS[i]) >= 0){
+				return i;
+			}
+		}
+		return STATUS_ORDERS.length;
+	};
+
+	const inBugListPage = () => {
+		return WORKSPACE_ID && location.href.indexOf('bugtrace/bugreports') > 0;
+	};
+
+	const getBugTotalCount = () => {
+		let $filter_result_cnt = $('#filter-result-count');
+		let $all_bug_cnt = $('#all_bug_count');
+		let cnt = $filter_result_cnt.size() ? $filter_result_cnt.html() : $all_bug_cnt.html();
+		return parseInt(cnt, 10);
+	};
+
+	const getBugTotalPage = () => {
+		let ph = $('#simple_pager_div .current-page').html();
+		if(!ph){
+			return 1;
+		}
+		return parseInt(ph.split('/')[1], 10);
+	};
+
+	/**
+	 * 获取bug完成评估时间
+	 * @param bug
+	 * @param change_list
+	 */
+	const getBugFinAssessTime = (bug, change_list)=>{
+		let start_time = getBugStartTime(change_list);
+		if(!start_time){
+			return null;
+		}
+
+		let time = null;
+		for(let i=0; i<change_list.length; i++){
+			if(change_list[i].type === '状态' && change_list[i].after.indexOf('待评估') >= 0){ //最后一次完成评估时间为准
+				time = change_list[i].datetime;
+				break;
+			}
+		}
+		if(!time){
+			return null;
+		}
+		return (Date.parse(time) - Date.parse(start_time))/1000;
+	};
+
+	const getBugFinTime = (bug, change_list)=>{
+		let start_time = getBugStartTime(change_list);
+		if(!start_time){
+			console.warn('no start time resolved', change_list);
+			return null;
+		}
+		if(bug.status.indexOf('关闭') < 0){
+			return null;
+		}
+		let time = null;
+		for(let i = 0; i < change_list.length; i++){
+			if(change_list[i].type === '状态' && change_list[i].after.indexOf('已关闭') >= 0){ //最后一次完成评估时间为准
+				time = change_list[i].datetime;
+				break;
+			}
+		}
+		if(!time){
+			return null;
+		}
+		return (Date.parse(time) - Date.parse(start_time)) / 1000;
+	};
+
+	const getBugStartTime = (change_list)=>{
+		if(!change_list.length){
+			return null;
+		}
+		return change_list[change_list.length-1].datetime;
+	};
+
+	/**
+	 * 通用的打开下载对话框方法，没有测试过具体兼容性
+	 * @param url 下载地址，也可以是一个blob对象，必选
+	 * @param saveName 保存文件名，可选
+	 */
+	function openDownloadDialog(url, saveName){
+		if(typeof url == 'object' && url instanceof Blob){
+			url = URL.createObjectURL(url); // 创建blob地址
+		}
+		let aLink = document.createElement('a');
+		aLink.href = url;
+		aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+		let event;
+		if(window.MouseEvent) event = new MouseEvent('click');
+		else{
+			event = document.createEvent('MouseEvents');
+			event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		}
+		aLink.dispatchEvent(event);
+	}
+
+	/**
+	 * 字符串转ArrayBuffer
+	 * @param s
+	 * @returns {ArrayBuffer}
+	 */
+	function s2ab(s) {
+		let buf = new ArrayBuffer(s.length);
+		let view = new Uint8Array(buf);
+		for (let i=0; i!==s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+		return buf;
+	}
+
+	/**
+	 * 将一个sheet转成最终的excel文件的blob对象，然后利用URL.createObjectURL下载
+	 * @param sheet
+	 * @param sheetName
+	 * @returns {Blob}
+	 */
+	function sheet2blob(sheet, sheetName) {
+		sheetName = sheetName || 'sheet1';
+		let workbook = {
+			SheetNames: [sheetName],
+			Sheets: {}
+		};
+		workbook.Sheets[sheetName] = sheet;
+		// 生成excel的配置项
+		let wopts = {
+			bookType: 'xlsx', // 要生成的文件类型
+			bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+			type: 'binary'
+		};
+		let wbout = XLSX.write(workbook, wopts);
+		return new Blob([s2ab(wbout)], {type:"application/octet-stream"});
+	}
+
+	const downloadTable = ($table, filename) => {
+		let aoa = [];
+		let row = [];
+		$table.find('thead>tr>th').each(function(){
+			let $th = $(this);
+			row.push($th.text());
+		});
+		aoa.push(row);
+
+		$table.find('tbody>tr').each(function(){
+			let row = [];
+			$(this).find('td').each(function(){
+				row.push($(this).text());
+			});
+			aoa.push(row);
+		});
+		let sheet = XLSX.utils.aoa_to_sheet(aoa);
+		openDownloadDialog(sheet2blob(sheet), filename);
+	};
+
+	const get_bug_page_size = ()=>{
+		let ph = $('#num-per-page .current').html();
+		if(!ph){
+			return 0;
+		}
+		return parseInt(ph, 10);
+	};
+
+	const getBugSummaryInfo = ()=>{
+		return {
+			total: getBugTotalCount(),
+			page: getBugTotalPage(),
+			page_size: get_bug_page_size(),
+		}
+	};
+
+	const getPageUrl = (page = 1) => {
+		let url = location.href;
+		if(url.indexOf("&page=") <= 0){
+			return url + (url.indexOf('?') >= 0 ? '&' : '?') + `page=${page}`;
+		}
+		return url.replace(/(&page=)\d+/i, "$1" + page);
+	};
+
+	const resolve_bug_info = (html)=>{
+		let tmp = /var\s+default_value\s*=\s*(.*)/i.exec(html);
+		let a;
+		try {
+			eval(`a = ${tmp[1]};`);
+		} catch(err){
+			console.error(err);
+		}
+		return a;
+	};
+
+	const query_selector = (html, selector)=>{
+		let $ret;
+		let $nodes = $(html);
+		$nodes.each(function(){
+			let $tmp = $(this).find(selector);
+			if($tmp.size()){
+				$ret = $tmp;
+				return false;
+			}
+		});
+		return $ret;
+	};
+
+	const getInfoUrl = (bug_id) => {
+		return `https://www.tapd.cn/${WORKSPACE_ID}/bugtrace/bugs/view?bug_id=${bug_id}`;
+	};
+
+	const loadPage = (url) => {
+		return new Promise((resolve, reject) => {
+			console.log('loading page', url);
+			fetch(url)
+				.then(data => data.text())
+				.then(html => {
+					console.info('page loaded, html size:'+html.length);
+					resolve(html);
+				});
+		});
+	};
+
+	const resolveFlowInfo = ($body, to_status_text) => {
+		let info = [];
+		$('#comments .field-active').get().reverse().each(function(){
+			let html = $(this).html();
+			if(html.indexOf('在流转') > 0 && html.indexOf('-' + to_status_text) > 0){
+				info = [$(this).parent().find('.field-time').text(), $(this).parent().find('.field-author').text()];
+				return false;
+			}
+		});
+		return info;
+	};
+
+	const getChangeList = (bug_id) => {
+		let url = `https://www.tapd.cn/${WORKSPACE_ID}/bugtrace/bugs/changes_list?perpage=100&bug_id=${bug_id}&time=` + (new Date()).getTime();
+		return new Promise((resolve, reject) => {
+			console.log('start fetch change list', url);
+			let xhr = new XMLHttpRequest();
+			xhr.open('GET', url);
+			xhr.send();
+			xhr.onload = function(e){
+				if(xhr.status === 200){
+					let changes = [];
+					let $html = $(xhr.responseText);
+					$html.find('.table-history>tbody>tr').each(function(){
+						let datetime = $(this).find('td').eq(1).text();
+
+						$('table tbody tr', this).each(function(){
+							let type = $(this).find('td').eq(0).text().trim();
+							let bf = $(this).find('td').eq(1).text();
+							let af = $(this).find('td').eq(2).text();
+							changes.push({
+								datetime: datetime,
+								date: formatDate(new Date(Date.parse(datetime)),'Y-m-d'),
+								type: type,
+								before: bf,
+								after: af
+							});
+						});
+					});
+					resolve(changes);
+				}else{
+					reject(`get change list fail: ${xhr.statusText} [${xhr.status}]`);
+					console.error('get change list fail:', e);
+				}
+			};
+		});
+	};
+
+	const getCommentList = ($comment_contents) => {
+		let comments = [];
+		$comment_contents.each(function(){
+			comments.push({
+				author:$.trim($(this).find('.field-author').text()),
+				time: $.trim($(this).find('.field-time').text()),
+				content: $.trim($(this).find('.editor-content').text())
+			});
+		});
+		return comments;
+	};
+
+	const resolveTextFromHtml = (html)=>{
+		let $tmp = $('<div style="display:none"></div>').html(html);
+		['style', 'script'].forEach(tag => $tmp.find(tag).remove());
+		return $tmp.text();
+	};
+
+	const getBugList = (page) => {
+		return new Promise((resolve, reject) => {
+			loadPage(getPageUrl(page)).then(html => {
+				let bug_list = [];
+				let $trs = query_selector(html, '#bug_list_content>tbody>tr');
+				if(!$trs){
+					reject('No dom node queried');
+					return;
+				}
+
+				let left_length = $trs.size();
+				let check = () => {
+					if(left_length > 0){
+						return;
+					}
+					console.log('bug list loaded', bug_list);
+					resolve(bug_list);
+				};
+
+				$trs.each(function(){
+					let title = $.trim($('a.namecol', this).text());
+					let href = $('a.namecol', this).attr('href');
+					let bug_id = $(this).attr('bug_id');
+					let current_owner = $(this).find('td[data-editable-field=current_owner] span').attr('title');
+					let status = $(this).find(`#bug_workflow_${bug_id}`).attr('title');
+					status = status.replace(/（[^）]+）/, '');
+
+					loadPage(getInfoUrl(bug_id)).then(html => {
+						let $cmt_wrap = query_selector(html, '#comment_area');
+						if(!$cmt_wrap){
+							reject('No dom node queried');
+							return;
+						}
+						let comments = getCommentList($cmt_wrap.find('.comment_content'));
+						console.log('bug comments got', bug_id, comments.length);
+
+						let info = resolve_bug_info(html);
+						let content_html = info.description;
+						let content_text = resolveTextFromHtml(info.description);
+						if(!content_html){
+							reject('no description html found');
+							return false;
+						}
+
+						let bug = {
+							id: bug_id,
+							title: title,
+							link: href,
+							bug_type: info.bugtype,
+							create_at: info.created,
+							content_text: content_text,
+							content_html: content_html,
+							owner: current_owner,
+							comments: comments,
+							status: status
+						};
+						console.info('bug resolved', bug);
+						bug_list.push(bug);
+						left_length--;
+						check();
+					});
+				});
+			}, reject);
+		});
+	};
+
+	const getBugChangeListCache = (bug_id)=>{
+		return TAPD_HELPER_BASE.cache(bug_id, ()=>{
+			return new Promise((resolve, reject) => {
+				TAPD_HELPER_BASE.getChangeList(bug_id).then(change_list=>{
+					console.log('change list for bug', bug_id, change_list);
+					resolve(change_list);
+				});
+			});
+		})
+	};
+
+	const WxWorkMsgType = {
+		Markdown: "markdown",
+		Text: "text",
+		Image: "image"
+	};
+
+	const sendToWxWorkRobot = (web_hook_url,
+	    content = {type: WxWorkMsgType.Markdown, content: ""},
+	    mentions = {user_id_list: [], mobile_list: []}) => {
+		chrome.extension.sendMessage({
+			type:'RemoteRequest',
+			url: web_hook_url,
+			data: content
+		}, (rsp, err)=>{
+			console.warn(rsp, err);
+		});
+	};
+
+	window.TAPD_HELPER_BASE = {
+		ONE_DAY: ONE_DAY,
+		ONE_HOUR: ONE_HOUR,
+		ONE_MIN: ONE_MIN,
+		WORKSPACE_ID: WORKSPACE_ID,
+		cache: cache,
+		downloadTable: downloadTable,
+		formatDate: formatDate,
+		calcIndex: calcIndex,
+		inBugListPage: inBugListPage,
+		getBugTotalCount: getBugTotalCount,
+		getBugFinAssessTime: getBugFinAssessTime,
+		getBugFinTime: getBugFinTime,
+		getBugTotalPage: getBugTotalPage,
+		getBugSummaryInfo: getBugSummaryInfo,
+		getPageUrl: getPageUrl,
+		getInfoUrl: getInfoUrl,
+		loadPage: loadPage,
+		getFlowInfo: resolveFlowInfo,
+		getChangeList: getChangeList,
+		getBugChangeListCache: getBugChangeListCache,
+		getBugList: getBugList,
+		prettyTimeRange: prettyTimeRange,
+		escapeHtml: escapeHtml,
+		sendToWxWorkRobot: sendToWxWorkRobot
+	};
+})();
---
+++ assert/js/content.run.js
@@ -0,0 +1,483 @@
+(function(){
+	const SUM_INFO = TAPD_HELPER_BASE.getBugSummaryInfo();
+	const CLASS_PREFIX = 'tapd-free-man';
+
+	let panel_html = `<dialog class="${CLASS_PREFIX}-panel" style="display:none;">
+					<div class="${CLASS_PREFIX}-panel-title">统计结果</div>
+					<div class="${CLASS_PREFIX}-panel-close-btn"></div>
+					<div class="${CLASS_PREFIX}-panel-content">
+						<div class="${CLASS_PREFIX}">
+							<div class="${CLASS_PREFIX}-result-op-wrap">
+								<span class="btn ${CLASS_PREFIX}-copy-export-data-btn"><span>保存数据</span></span>
+							</div>
+							<div class="${CLASS_PREFIX}-sum">当前条件查询工单 ${SUM_INFO.total} 条，共 ${SUM_INFO.page} 页，每页 ${SUM_INFO.page_size} 条。</div>
+							<div class="${CLASS_PREFIX}-pg-wrap" style="display:none;">
+								<div class="${CLASS_PREFIX}-pg-tip"></div>
+							</div>
+							<div class="${CLASS_PREFIX}-op-wrap">
+								<span class="${CLASS_PREFIX}-btn btn"><span>开始分析</span></span>
+							</div>
+							<ul class="${CLASS_PREFIX}-status-sum" style="display:none"></ul>
+							<div class="${CLASS_PREFIX}-flow-chart" style="display:none; height:200px;"></div>
+							<div class="${CLASS_PREFIX}-sub-chart-wrap" style="display:none;"></div>
+							<div class="${CLASS_PREFIX}-export-data" style="display:none;">
+								<table class="${CLASS_PREFIX}-table">
+									<caption>数据导出预览 </caption>
+									<thead><tr></tr></thead>
+									<tbody></tbody>
+								</table>
+							</div>
+							<table class="${CLASS_PREFIX}-table ${CLASS_PREFIX}-lazy-trans-top" style="display:none">
+								<caption>未扭转工单排名</caption>
+								<tbody></tbody>
+							</table>
+							<table class="${CLASS_PREFIX}-table ${CLASS_PREFIX}-unclosed-top" style="display:none;">
+								<caption>未关闭工单排名</caption>
+								<tbody></tbody>
+							</table>
+						</div>
+					</div>
+				</dialog>`;
+
+	let $panel = $(panel_html).appendTo('body');
+	let $sum_tip = $panel.find(`.${CLASS_PREFIX}-sum`);
+	let $start_btn = $panel.find(`.${CLASS_PREFIX}-btn`);
+	let $op_wrap = $panel.find(`.${CLASS_PREFIX}-op-wrap`);
+	let $pg_wrap = $panel.find(`.${CLASS_PREFIX}-pg-wrap`);
+	let $pg_tip = $pg_wrap.find(`.${CLASS_PREFIX}-pg-tip`);
+	let $status_sum = $panel.find(`.${CLASS_PREFIX}-status-sum`);
+	let $lazy_trans_top = $panel.find(`.${CLASS_PREFIX}-lazy-trans-top`);
+	let $unclosed_top = $panel.find(`.${CLASS_PREFIX}-unclosed-top`);
+	let $sub_charts = $panel.find(`.${CLASS_PREFIX}-sub-chart-wrap`);
+	let $send_to_robot_btn = $panel.find(`.${CLASS_PREFIX}-send-to-robot`);
+	let $export_data = $panel.find(`.${CLASS_PREFIX}-export-data`);
+	let $copy_export_data_btn = $panel.find(`.${CLASS_PREFIX}-copy-export-data-btn`);
+
+	let current_page = 1;
+	let stop_flag = false;
+	let total_bug_groups = {};
+	let total_change_groups = {};
+
+	const getExportFields = (()=>{
+		let export_fields = null;
+		return function(cb){
+			if(export_fields){
+				cb(export_fields);
+			} else {
+				TAPD_HELPER_CHROME.sendMessageToBackground({type:'getExportFields'}, cb);
+			}
+		};
+	})();
+
+	$copy_export_data_btn.click(function(){
+		TAPD_HELPER_BASE.downloadTable($export_data, '工单统计' + TAPD_HELPER_BASE.formatDate(new Date(), 'Ymd')+'.xlsx');
+	});
+
+	$panel.find(`.${CLASS_PREFIX}-panel-close-btn`).click(() => {
+		stop();
+		$panel && $panel.hide();
+	});
+
+	if(location.hash.indexOf('tapd_helper_autostart') >= 0){
+		location.hash = '';
+		show_panel();
+		start();
+	}
+
+	TAPD_HELPER_CHROME.onMessage(function(request, sender, sendResponse){
+		console.log('content on message', request);
+		switch(request){
+			case 'TAPD_START_ANALLY':
+				if(show_panel()){
+					start();
+				}
+				sendResponse();
+				break;
+
+			case 'TAPD_ON_SUPPORT':
+				sendResponse(!!TAPD_HELPER_BASE.WORKSPACE_ID && /\/bugtrace\//i.test(location.href));
+				break;
+			default:
+				console.error(request);
+				throw "No request type support";
+		}
+	});
+
+	$start_btn.click(start);
+
+	$send_to_robot_btn.click(()=>{
+		TAPD_HELPER_BASE.sendToWxWorkRobot()
+	});
+
+	function start(){
+		$sum_tip.hide();
+		$pg_wrap.show();
+		$start_btn.addClass('.disabled');
+		if($start_btn.text() === '开始分析'){
+			current_page = 1;
+			stop_flag = false;
+			$status_sum.html('');
+			analyze_page();
+		}
+		else if($start_btn.text() === '重新分析'){
+			location.hash = '#tapd_helper_autostart';
+			location.reload();
+		}
+		else {
+			$start_btn.html('<span>重新分析</span>');
+			stop_flag = true;
+		}
+	}
+
+	function stop(){
+		stop_flag = true;
+	}
+
+	function add_data_tbl(bug){
+		getExportFields(export_fields=>{
+			if(!$export_data.find('thead th').size()){
+				let th_html = '';
+				for(let k in export_fields){
+					if(k === 'comment_list'){
+						th_html += `<th>评论1</th><th>评论2</th><th>评论3</th><th>评论4</th><th>评论5</th><th>评论6</th>`;
+					} else {
+						th_html += `<th>${export_fields[k]}</th>`;
+					}
+				}
+				$export_data.find('thead tr').html(th_html);
+			}
+
+			let html = '<tr>';
+			for(let k in export_fields){
+				if(k === 'comment_list'){
+					let comment_html_list = '';
+					bug.comments.forEach(cmt=>{
+						comment_html_list +=
+							`<td>
+								${TAPD_HELPER_BASE.escapeHtml(cmt.author)}（${cmt.time}）：
+								${TAPD_HELPER_BASE.escapeHtml(cmt.content)}
+							</td>`;
+					});
+					html += comment_html_list;
+					continue;
+				}
+				if(bug[k] !== null){
+					html += `<td>${TAPD_HELPER_BASE.escapeHtml(bug[k])}</td>`;
+				}
+			}
+			html += '</tr>';
+			$(html).appendTo($export_data.find('tbody'));
+		});
+	}
+
+	function add_status_sum(bug){
+		$status_sum.show();
+		let found = false;
+		$status_sum.find('li').each(function(){
+			let $li = $(this);
+			if($li.data('status') === bug.status){
+				let cnt = parseInt($li.data('cnt'), 10) + 1;
+				$li.find('.cnt').html(cnt);
+				$li.data('cnt', cnt);
+				found = true;
+				return false;
+			}
+		});
+		if(!found){
+			let idx = TAPD_HELPER_BASE.calcIndex(bug.status);
+			let html = `<li data-cnt="1" data-status="${bug.status}"><span class="cnt">1</span><span class="status">${bug.status}</span></li>`;
+			let $last = null;
+			$status_sum.find('li').each(function(){
+				let li_status = $(this).data('status');
+				if(TAPD_HELPER_BASE.calcIndex(li_status) >= idx){
+					return false;
+				}
+				$last = $(this);
+			});
+			if($last){
+				$(html).insertAfter($last);
+			} else {
+				$(html).prependTo($status_sum);
+			}
+		}
+	}
+
+	function name_list(name_str){
+		let ns = name_str.split(';');
+		let tmp = [];
+		ns.forEach(n=>{
+			n = $.trim(n);
+			if(n){
+				tmp.push(`<span class="n">${TAPD_HELPER_BASE.escapeHtml(n)}</span>`);
+			}
+		});
+		return tmp.join('');
+	}
+
+	function add_lazy_trans_top(bug, change_list){
+		if(bug.status !== '待评估'){
+			return;
+		}
+		let last_status_time = 0;
+		change_list.forEach(change=>{
+			if(change.type === '状态'){
+				last_status_time = change.datetime;
+				return false;
+			}
+		});
+
+		if(!last_status_time){
+			return;
+		}
+
+		let offset = (new Date().getTime()) -  Date.parse(last_status_time);
+		let found = false;
+		let html = `<tr data-offset="${offset}">
+						<td><a href="${bug.link}" target="_blank" class="ti" title="${TAPD_HELPER_BASE.escapeHtml(bug.title)}">${TAPD_HELPER_BASE.escapeHtml(bug.title)}</a></td>
+						<td>${name_list(bug.owner)}</td>
+						<td class="nowrap">${bug.status}</td>
+						<td class="nowrap">${TAPD_HELPER_BASE.prettyTimeRange(offset)}</td>
+					</tr>`;
+		$lazy_trans_top.find('tr').each(function(){
+			if($(this).data('offset') < offset){
+				$(html).insertBefore(this);
+				found = true;
+				return false;
+			}
+		});
+		if(!found){
+			$(html).appendTo($lazy_trans_top.find('tbody'));
+		}
+		$lazy_trans_top.show();
+	}
+
+	function add_unclosed_top(bug, change_list){
+		let close_status_list = ['已关闭', '已转需求', '已拒绝', '待回电'];
+		if(!change_list.length){
+			console.warn('no change list found', bug);
+			return;
+		}
+		if(close_status_list.indexOf(bug.status) >= 0){
+			return;
+		}
+		let create_time = change_list[0].datetime;
+
+		let offset = (new Date().getTime()) -  Date.parse(create_time);
+		let found = false;
+		let html = `<tr data-offset="${offset}">
+						<td><a href="${bug.link}" title="${TAPD_HELPER_BASE.escapeHtml(bug.title)}" target="_blank" class="ti">${TAPD_HELPER_BASE.escapeHtml(bug.title)}</a></td>
+						<td>${name_list(bug.owner)}</td>
+						<td class="nowrap">${bug.status}</td>
+						<td class="nowrap">${TAPD_HELPER_BASE.prettyTimeRange(offset)}</td>
+					</tr>`;
+		$unclosed_top.find('tr').each(function(){
+			if($(this).data('offset') < offset){
+				$(html).insertBefore(this);
+				found = true;
+				return false;
+			}
+		});
+		if(!found){
+			$(html).appendTo($unclosed_top.find('tbody'));
+		}
+		$unclosed_top.show();
+	}
+
+	function analyze_page(){
+		if(current_page > SUM_INFO.page){
+			$pg_tip.html(`分析完成`);
+			$start_btn.find('span').html('<span>重新分析</span>');
+			return;
+		}
+		if(stop_flag){
+			stop_flag = false;
+			return;
+		}
+		$start_btn.html('<span>停止分析</span>');
+		let idx = (current_page-1)*SUM_INFO.page_size;
+		$pg_tip.html(`[${idx}/${SUM_INFO.total}] 正在分析第 ${current_page} 页数据...`);
+
+		console.info('start analyze page');
+		TAPD_HELPER_BASE.cache('bug_list_'+current_page, ()=>{
+			return TAPD_HELPER_BASE.getBugList(current_page);
+		}).then(bug_list=>{
+			console.info('bug list got:', bug_list.length);
+			bug_list.forEach(bug=>{
+				total_bug_groups[bug.id] = bug;
+			});
+			let pt = bug_list.length;
+			let fd = ()=>{
+				let bug = bug_list.shift();
+				if(!bug || stop_flag){
+					current_page++;
+					analyze_page();
+					return;
+				}
+				add_status_sum(bug);
+				add_data_tbl(bug);
+				let idx = (current_page-1)*SUM_INFO.page_size + (pt-bug_list.length);
+				$pg_tip.html(`[${idx}/${SUM_INFO.total}] 正在获取工单信息：<a href="${bug.link}" target="_blank">${bug.title}</a>`);
+				TAPD_HELPER_BASE.getBugChangeListCache(bug.id).then(change_list=>{
+					total_change_groups[bug.id] = change_list;
+					update_flow_charts();
+					update_sub_charts();
+					add_lazy_trans_top(bug, change_list);
+					add_unclosed_top(bug, change_list);
+					fd();
+				});
+			};
+			fd();
+		});
+	}
+
+	function show_panel(){
+		let show_flag = !$panel.is(':visible');
+		$panel.show();
+		return show_flag;
+	}
+
+	const update_sub_charts = ()=>{
+		//24小时完成率
+		let fin_ass_in_24_count = 0,
+			fin_ass_in_48_count = 0,
+			fin_in_120_count = 0,
+			fin_in_240_count = 0;
+
+		let total = 0;
+		for(let bug_id in total_change_groups){
+			total++;
+			let bug = total_bug_groups[bug_id];
+			let changes = total_change_groups[bug_id];
+			if(TAPD_HELPER_BASE.getBugFinAssessTime(bug, changes) <= TAPD_HELPER_BASE.ONE_DAY){
+				fin_ass_in_24_count++;
+			}
+			if(TAPD_HELPER_BASE.getBugFinAssessTime(bug, changes) <= (TAPD_HELPER_BASE.ONE_DAY*2)){
+				fin_ass_in_48_count++;
+			}
+			if(TAPD_HELPER_BASE.getBugFinTime(bug, changes) !== null && TAPD_HELPER_BASE.getBugFinTime(bug, changes) <= (TAPD_HELPER_BASE.ONE_DAY*5)){
+				fin_in_120_count++;
+			}
+			if(TAPD_HELPER_BASE.getBugFinTime(bug, changes) !== null && TAPD_HELPER_BASE.getBugFinTime(bug, changes) <= (TAPD_HELPER_BASE.ONE_DAY*10)){
+				fin_in_240_count++;
+			}
+		}
+
+		let html = '';
+		let makePercent = (percent,title)=>`<span class="${CLASS_PREFIX}-sub-chart">
+				<s>${percent}%</s>
+				<span>${title}</span>
+				<progress max="100" value="${percent}"></progress>
+				</span>`;
+		html += makePercent(Math.round(100 * fin_ass_in_24_count / total), '24小时完成评估');
+		html += makePercent(Math.round(100 * fin_ass_in_48_count / total), '48小时完成评估');
+		html += makePercent(Math.round(100 * fin_in_120_count / total), '120小时工单关闭');
+		html += makePercent(Math.round(100 * fin_in_240_count / total), '240小时工单关闭');
+		$sub_charts.html(html).show();
+	};
+
+	const update_flow_charts = () => {
+		//draw flow chart
+		let $chart = $panel.find(`.${CLASS_PREFIX}-flow-chart`).show();
+		let date_serials = [];
+		let status_groups = {}; // status1->date1= 1, date1->status2 = 1,
+		for(let bug_id in total_change_groups){
+			let changes = total_change_groups[bug_id];
+			let tmp = {};
+			changes.forEach(change=>{
+				if(change.type === '状态' && !tmp[change.date]){ //每个工单，每天只统计最后一次变更的状态
+					date_serials.push(TAPD_HELPER_BASE.formatDate(new Date(Date.parse(change.date)), 'm/d'));
+					tmp[change.date] = true;
+					if(!status_groups[change.after]){
+						status_groups[change.after] = {[change.date]:1};
+					} else {
+						status_groups[change.after][change.date] = (status_groups[change.after][change.date] || 0)+1;
+					}
+				}
+			});
+		}
+
+		date_serials = date_serials.filter((value, index, self)=>{return self.indexOf(value) === index;});
+		date_serials = date_serials.sort((v1, v2)=>{
+			let t1 = Date.parse(v1);
+			let t2 = Date.parse(v2);
+			if(t1 === t2){
+				return 0;
+			}
+			return t1 > t2 ? 1 : -1;
+		});
+
+		let series = [];
+		let status_categories = [];
+		for(let status in status_groups){
+			status_categories.push(status.replace(/（[^）]+）/, ''));
+			let dm = status_groups[status];
+			let tmp = [];
+			for(let date in dm){
+				tmp.push({date:date, count:dm[date]});
+			}
+			tmp = tmp.sort((v1,v2)=>{
+				let t1 = Date.parse(v1.date);
+				let t2 = Date.parse(v2.date);
+				if(t1 === t2){
+					return 0;
+				}
+				return t1 > t2 ? 1 : -1;
+			});
+			let count_serials = [];
+			tmp.forEach(obj=>{
+				count_serials.push(obj.count);
+			});
+			series.push({
+				name: status.replace(/（[^）]+）/, ''),
+				type: 'line',
+				stack: '总量',
+				areaStyle: [],
+				data: count_serials
+			});
+		}
+
+		let myChart = echarts.init($chart[0]);
+		let option = {
+			title: {
+				text: ''
+			},
+			tooltip: {
+				trigger: 'axis',
+				axisPointer: {
+					type: 'cross',
+					label: {
+						backgroundColor: '#6a7985'
+					}
+				}
+			},
+			legend: {
+				data: status_categories
+			},
+			toolbox: {
+				feature: {
+					saveAsImage: {}
+				}
+			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '3%',
+				containLabel: true
+			},
+			xAxis: [
+				{
+					type: 'category',
+					boundaryGap: false,
+					data: date_serials
+				}
+			],
+			yAxis: [
+				{
+					type: 'value'
+				}
+			],
+			series: series
+		};
+		myChart.setOption(option);
+	};
+})();
